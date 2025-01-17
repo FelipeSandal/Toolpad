3b6847b0238d0f5d556020e770f50e849d30f624
FelipeSandal <felipesandal123@gmail.com>
2025-01-17 15:13:50 +0100

    Grid med navn, bilde og info

---
+++ app/(dashboard)/search/page.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function SearchPage() {
+  return <Typography>Welcome to the Toolpad Search!</Typography>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,55 @@
-import * as React from 'react';
-import { NextAppProvider } from '@toolpad/core/nextjs';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LinearProgress from '@mui/material/LinearProgress'
-import type { Navigation } from '@toolpad/core/AppProvider';
+import * as React from "react";
+import { NextAppProvider } from "@toolpad/core/nextjs";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LinearProgress from "@mui/material/LinearProgress";
+import type { Navigation } from "@toolpad/core/AppProvider";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
-import theme from '../theme';
+import theme from "../theme";
 
 const NAVIGATION: Navigation = [
   {
-    kind: 'header',
-    title: 'Main items',
+    kind: "header",
+    title: "System",
   },
   {
-    segment: '',
-    title: 'Dashboard',
-    icon: <DashboardIcon />,
+    segment: "",
+    title: "System",
+    icon: <HomeIcon />,
   },
   {
-    segment: 'orders',
-    title: 'Orders',
-    icon: <ShoppingCartIcon />,
+    segment: "search",
+    title: "Search",
+    icon: <SearchIcon />,
+  },
+  {
+    segment: "accounting",
+    title: "Accounting",
+    icon: <AccountBalanceIcon />,
   },
 ];
 
 const BRANDING = {
-  title: 'My Toolpad Core Next.js App',
+  title: "My Toolpad Core Next.js App",
 };
 
-
-
 export default function RootLayout(props: { children: React.ReactNode }) {
-  
-
   return (
-    <html lang="en" data-toolpad-color-scheme="light" suppressHydrationWarning>
+    <html lang="en" data-toolpad-color-scheme="dark" suppressHydrationWarning>
       <body>
-        
-          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <React.Suspense fallback={<LinearProgress />}>
             <NextAppProvider
               navigation={NAVIGATION}
               branding={BRANDING}
-              
               theme={theme}
             >
               {props.children}
             </NextAppProvider>
-            </React.Suspense>
-          </AppRouterCacheProvider>
-        
+          </React.Suspense>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
